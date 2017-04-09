a8e399b500e4ab731298b8167e7ce957dac6469b
Zhou <zcz@t61>
2017-04-09 10:00:03 +0800

    修改simple driver，支持offset读写，添加 gdb脚本，busybox制作，qemu启动script

---
+++ linux_drv_dev/README.md
@@ -0,0 +1,3 @@
+
+1. qemu\_script:   scripts for qemu, like make busybox image file, initiate qemu with so many arguments
+1. simple\_driver: simple driver framework, support read/write with offset
--- linux_drv_dev/simple_driver/simple_drv.c
+++ linux_drv_dev/simple_driver/simple_drv.c
@@ -20,16 +20,17 @@
 #include <linux/vmalloc.h>
 #include <linux/mutex.h>
 
-#define DEVICE_NAME	"simple_drv"
+#define DEVICE_NAME "simple_drv"
 
 #define BUFFER_SIZ 32
 unsigned char dev_buffer[BUFFER_SIZ] = {0};
 
 struct simple_drv_ext
 {
+    struct miscdevice dev_miscdev;
+    unsigned int current_pointer;
     int write_count;
     int read_count;
-    struct miscdevice dev_miscdev;
 };
 
 /*--------------------------------------------------------------------------------------------------------
@@ -43,20 +44,20 @@ static int simple_drv_init(void);
 static void simple_drv_exit(void);
 
 static struct file_operations simple_drv_fops={
-	.owner		= THIS_MODULE,
-	.open 		= simple_drv_open,
-	.write		= simple_drv_write,
+    .owner      = THIS_MODULE,
+    .open       = simple_drv_open,
+    .write      = simple_drv_write,
     .read       = simple_drv_read,
-	.release	= simple_drv_release,
-	//.ioctl		= simple_drv_ioctl,
+    .release    = simple_drv_release,
 };
 
 struct simple_drv_ext my_dev_ext = {
+    .current_pointer    = 0,
     .write_count = 0,
     .read_count = 0,
     .dev_miscdev.minor  = MISC_DYNAMIC_MINOR,
-    .dev_miscdev.name	= DEVICE_NAME,
-    .dev_miscdev.fops	= &simple_drv_fops,
+    .dev_miscdev.name    = DEVICE_NAME,
+    .dev_miscdev.fops    = &simple_drv_fops,
 };
 
 /*--------------------------------------------------------------------------------------------------------
@@ -64,13 +65,13 @@ struct simple_drv_ext my_dev_ext = {
 static int simple_drv_open(struct inode *inode, struct file *filp)
 {
     printk("open %08X: f_flags: %08X\n", (unsigned int)simple_drv_open, filp->f_flags);
-	return 0;
+    return 0;
 }
 
 static int  simple_drv_release(struct inode *inode, struct file *filp)
 {
     printk("release %08X: \n", (unsigned int)simple_drv_release);
-	return 0;
+    return 0;
 }
 
 ssize_t simple_drv_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
@@ -79,11 +80,24 @@ ssize_t simple_drv_read(struct file *filp, char __user *buf, size_t count, loff_
     my_dev_ext.read_count++;
     printk("simple_drv_read %08X up, read count %d\n", (unsigned int)simple_drv_read, my_dev_ext.read_count);
 
-    siz = count < BUFFER_SIZ ? count : BUFFER_SIZ;
-    if (copy_to_user((void *)buf, dev_buffer, siz))
+    if (count == 0)
+        return 0;
+
+    if (*f_pos >= BUFFER_SIZ)
+        return -EINVAL;
+    if (*f_pos + count > BUFFER_SIZ)
+    {
+        siz = BUFFER_SIZ - *f_pos;
+    }
+    else
+    {
+        siz = count;
+    }
+    if (copy_to_user((void *)buf, dev_buffer + *f_pos, siz))
     {
         return -EFAULT;
     }
+    *f_pos += siz;
 
     return siz;
 }
@@ -98,10 +112,22 @@ ssize_t simple_drv_write(struct file *filp, const char __user *buf, size_t count
     if (count == 0)
         return 0;
 
-    siz = (BUFFER_SIZ < count) ? BUFFER_SIZ : count;
-    rc = copy_from_user(dev_buffer, buf, siz);
+    if (*f_pos >= BUFFER_SIZ)
+        return -EINVAL;
+
+    if (*f_pos + count > BUFFER_SIZ)
+    {
+        siz = BUFFER_SIZ - *f_pos;
+    }
+    else
+    {
+        siz = count;
+    }
+
+    rc = copy_from_user(dev_buffer + *f_pos, buf, siz);
     if (rc == 0)
     {
+        *f_pos += siz;
         return siz;
     }
     else
@@ -112,35 +138,35 @@ ssize_t simple_drv_write(struct file *filp, const char __user *buf, size_t count
 
 int simple_drv_ioctl(struct inode *inode,struct file *flip,unsigned int command,unsigned long arg)
 {
-	switch (command) {
-	case 0: 
+    switch (command) {
+    case 0: 
         printk("Write 0 for LED_GPIO\n");
-		break;
-	
-	case 1:
+        break;
+    
+    case 1:
         printk("Write 1 for LED_GPIO\n");
-		break;
-	}
+        break;
+    }
 
-	return 0;
+    return 0;
 }
 
 static int __init simple_drv_init(void)
 {
-	misc_register(&my_dev_ext.dev_miscdev);
-	printk(DEVICE_NAME" up. \n"); 
+    misc_register(&my_dev_ext.dev_miscdev);
+    printk(DEVICE_NAME" up. \n"); 
     printk("init %08X\n", (unsigned int)simple_drv_init);
     printk("kobject->name        %s\n", my_dev_ext.dev_miscdev.this_device->kobj.name);
     //printk("parent kobject->name %s\n", my_dev_ext.dev_miscdev.parent->kobj.name);
 
-	return 0;
+    return 0;
 }
 
 static void __exit simple_drv_exit(void)
 {
     printk("exit %08X\n", (unsigned int)simple_drv_exit);
-	misc_deregister(&my_dev_ext.dev_miscdev);
-	printk(DEVICE_NAME " down.\n");
+    misc_deregister(&my_dev_ext.dev_miscdev);
+    printk(DEVICE_NAME " down.\n");
 }
 
 module_init(simple_drv_init);
